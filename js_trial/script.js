48f4fa234d0512b1c7a2ee73419ab390b067b318
Вадим Валыгин <vadim12388@mail.ru>
2022-10-13 17:51:05 +0300

    reduce

--- js_trial/script.js
+++ js_trial/script.js
@@ -1322,3 +1322,65 @@ const takeOldest = (users, count = 1) => {
 
 export default takeOldest;
 // END
+
+//
+// Реализуйте и экспортируйте по умолчанию функцию для группировки объектов по заданному свойству. Функция принимает аргументами массив объектов и название свойства для группировки. Она должна возвращать объект, где ключ - это значение по заданному свойству, а значение - массив с данными, подходящими для группы.
+//
+//     import groupBy from './groupBy.js';
+//
+// const students = [
+//     { name: 'Tirion', class: 'B', mark: 3 },
+//     { name: 'Keit', class: 'A', mark: 3 },
+//     { name: 'Ramsey', class: 'A', mark: 4 },
+// ];
+//
+// groupBy([], ''); // {}
+// groupBy(students, 'mark');
+// // {
+// //   3: [
+// //     { name: "Tirion", class: "B", mark: 3 },
+// //     { name: "Keit", class: "A", mark: 3 },
+// //   ],
+// //   4: [
+// //     { name: "Ramsey", class: "A", mark: 4 },
+// //   ],
+// // }
+// Подсказки
+// Аналогичная функция есть в lodash, но вам её нужно создать самостоятельно
+// Алгоритм решения задачи с помощью цикла и редьюса одинаковый. Если вам так проще, сделайте сначала через цикл, затем перепишите через reduce
+// Решение этой задачи аналогично решению задачи usersByAge из теории
+
+
+// мое
+// @ts-check
+
+// BEGIN (write your solution here)
+export default (students, key) => students.reduce((acc, student) => {
+    const studentKey = student[key];
+    if (!Array.isArray(acc[studentKey])) {
+        acc[studentKey] = [];
+    }
+    acc[studentKey].push(student);
+    return acc;
+}, {});
+// END
+
+// учителя
+
+// @ts-check
+
+// BEGIN
+const groupBy = (objects, key) => objects.reduce((acc, object) => {
+    // из каждого объекта берётся значение по ключу
+    const groupName = object[key];
+    // контейнером группы выступает массив
+    // Оператор нулевого слияния возвращает пустой массив, если в аккумуляторе ничего нет
+    const group = acc[groupName] ?? [];
+    // возвращается новый объект аккумулятора
+    // старый аккумулятор деструктурируется, для текущей группы записывается новый массив с данными
+    // квадратные скобки нужны, чтобы указать имя группы в качестве ключа
+    return { ...acc, [groupName]: group.concat(object) };
+}, {});
+
+export default groupBy;
+// END
