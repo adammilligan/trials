998ac49e7a482a0361b7f6122fdde6f824a7c031
Вадим Валыгин <vadim12388@mail.ru>
2022-10-27 14:04:25 +0300

    object detected

--- js_trial/script.js
+++ js_trial/script.js
@@ -1891,3 +1891,60 @@ test('set new property', () => {
     expect(data).toEqual(dataCopy);
 });
 // END
+
+//
+// Реализуйте и экспортируйте по умолчанию функцию, которая принимает на вход массив (элементы которого — это объекты) и пары ключ-значение (тоже в виде объекта), а возвращает первый элемент исходного массива, значения которого соответствуют переданным парам (всем переданным). Если совпадений не было, то функция должна вернуть null.
+//
+//     Примеры
+// findWhere(
+//     [
+//         { title: 'Book of Fooos', author: 'FooBar', year: 1111 },
+//         { title: 'Cymbeline', author: 'Shakespeare', year: 1611 },
+//         { title: 'The Tempest', author: 'Shakespeare', year: 1611 },
+//         { title: 'Book of Foos Barrrs', author: 'FooBar', year: 2222 },
+//         { title: 'Still foooing', author: 'FooBar', year: 3333 },
+//         { title: 'Happy Foo', author: 'FooBar', year: 4444 },
+//     ],
+//     { author: 'Shakespeare', year: 1611 }
+// ); // { title: 'Cymbeline', author: 'Shakespeare', year: 1611 }
+
+
+// моё
+// @ts-check
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+// BEGIN (write your solution here)
+export default (list, element) => {
+    for (const i of list) {
+        if (JSON.stringify({ ...i }) === JSON.stringify({ ...i, ...element })) {
+            return i;
+        }
+    }
+    return null;
+};
+
+// END
+
+// учителя
+
+// @ts-check
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+// BEGIN
+const findWhere = (data, where) => {
+    const entries = Object.entries(where);
+    for (const item of data) {
+        let find = true;
+        for (const [key, value] of entries) {
+            if (item[key] !== value) {
+                find = false;
+            }
+        }
+        if (find) {
+            return item;
+        }
+    }
+
+    return null;
+};
+
+export default findWhere;
+// END
