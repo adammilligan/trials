cc87543ae2aac5eb26e4b470aabd4e25452fe76f
Вадим Валыгин <vadim12388@mail.ru>
2022-10-27 16:11:27 +0300

    object diff

--- js_trial/script.js
+++ js_trial/script.js
@@ -1948,3 +1948,98 @@ const findWhere = (data, where) => {
 
 export default findWhere;
 // END
+
+
+// Иногда в программировании возникает задача поиска разницы между двумя наборами данных, такими как объекты. Например, при поиске различий в json файлах. Для этого даже существуют специальные сервисы, например, http://www.jsondiff.com/ (попробуйте нажать на ссылку sample data и затем кнопку Compare).
+//
+// solution.js
+// Реализуйте и экспортируйте по умолчанию функцию, которая сравнивает два объекта и возвращает результат сравнения в виде объекта. Ключами результирующего объекта будут все ключи из двух входящих объектов, а значением строка с описанием отличий: added, deleted, changed или unchanged.
+//
+//     Возможные значения:
+//
+//     added — ключ отсутствовал в первом объекте, но был добавлен во второй
+// deleted — ключ был в первом объекте, но отсутствует во втором
+// changed — ключ присутствовал и в первом и во втором объектах, но значения отличаются
+// unchanged — ключ присутствовал и в первом и во втором объектах с одинаковыми значениями
+// import genDiff from './diffGenerator.js';
+//
+// genDiff(
+//     { one: 'eon', two: 'two', four: true },
+//     { two: 'own', zero: 4, four: true },
+// );
+// // {
+// //   one: 'deleted',
+// //   two: 'changed',
+// //   four: 'unchanged',
+// //   zero: 'added',
+// // }
+
+
+
+// моё
+
+// @ts-check
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+
+// import _ from 'lodash';
+
+// BEGIN (write your solution here)
+export default (initialObject, resultObject) => {
+    const diff = {};
+    const initialEntries = Object.entries(initialObject);
+    const resultKeys = Object.keys(resultObject);
+    for (const [key, value] of initialEntries) {
+        if (Object.hasOwn(resultObject, key)) {
+            if (resultObject[key] === value) {
+                diff[key] = 'unchanged';
+            } else {
+                diff[key] = 'changed';
+            }
+        } else {
+            diff[key] = 'deleted';
+        }
+    }
+    for (const key of resultKeys) {
+        if (!Object.hasOwn(initialObject, key)) {
+            diff[key] = 'added';
+        }
+    }
+    return diff;
+};
+
+// END
+
+
+// учителя
+
+
+// @ts-check
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+
+import _ from 'lodash';
+
+// BEGIN
+
+const genDiff = (data1, data2) => {
+    const keys1 = Object.keys(data1);
+    const keys2 = Object.keys(data2);
+    const keys = _.union(keys1, keys2); // https://youtu.be/vkUTX1hruF8?t=929
+
+    const result = {};
+    for (const key of keys) {
+        if (!Object.hasOwn(data1, key)) {
+            result[key] = 'added';
+        } else if (!Object.hasOwn(data2, key)) {
+            result[key] = 'deleted';
+        } else if (data1[key] !== data2[key]) {
+            result[key] = 'changed';
+        } else {
+            result[key] = 'unchanged';
+        }
+    }
+
+    return result;
+};
+
+export default genDiff;
+// END
