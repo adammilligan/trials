738963947f2bb57ce57e3b39d2a07a2611613796
Вадим Валыгин <vadim12388@mail.ru>
2022-11-07 13:15:41 +0300

    virtual file sistem

--- js_trial/script.js
+++ js_trial/script.js
@@ -2245,3 +2245,81 @@ export const toArabic = (romanNumber) => {
     return result;
 };
 // END
+
+// tree.js
+// Реализуйте и экспортируйте функцию compressImages(), которая принимает на вход директорию, находит внутри нее картинки и "сжимает" их. Под сжиманием понимается уменьшение свойства size в метаданных в два раза. Функция должна вернуть новую директорию со сжатыми картинками и всеми остальными данными, которые были внутри этой директории.
+//
+//     Картинками считаются все файлы заканчивающиеся на .jpg.
+//
+//     Примеры
+// const tree = mkdir('my documents', [
+//     mkfile('avatar.jpg', { size: 100 }),
+//     mkfile('passport.jpg', { size: 200 }),
+//     mkfile('family.jpg', { size: 150 }),
+//     mkfile('addresses', { size: 125 }),
+//     mkdir('presentations')
+// ]);
+//
+// const newTree = compressImages(tree);
+// // То же самое, что и tree, но во всех картинках размер уменьшен в два раза
+
+// мое
+/* eslint-disable import/prefer-default-export */
+// @ts-check
+
+import _ from 'lodash';
+import {
+    mkdir, mkfile, isFile, getChildren, getName, getMeta,
+} from '@hexlet/immutable-fs-trees';
+
+// BEGIN (write your solution here)
+const compressImages = (tree) => {
+    const children = getChildren(tree);
+    const newChildren = children.map((child) => {
+        const newName = _.cloneDeep(getName(child));
+        const meta = _.cloneDeep(getMeta(child));
+        const newMeta = { ...meta, size: meta.size / 2 };
+        if (isFile(child)) {
+            if (child.name.includes('jpg')) {
+                return mkfile(newName, newMeta);
+            }
+            return mkfile(newName, meta);
+        }
+        return mkdir(newName, getChildren(child), meta);
+    });
+    const newMeta = _.cloneDeep(getMeta(tree));
+    const newTree = mkdir(getName(tree), newChildren, newMeta);
+    return newTree;
+};
+
+export { compressImages };
+// END
+
+
+// учителя
+
+/* eslint-disable import/prefer-default-export */
+// @ts-check
+
+import _ from 'lodash';
+import {
+    mkdir, mkfile, isFile, getChildren, getName, getMeta,
+} from '@hexlet/immutable-fs-trees';
+
+// BEGIN
+export const compressImages = (node) => {
+    const children = getChildren(node);
+    const newChildren = children.map((child) => {
+        const name = getName(child);
+        if (!isFile(child) || !name.endsWith('.jpg')) {
+            return child;
+        }
+        const meta = getMeta(child);
+        const newMeta = _.cloneDeep(meta);
+        newMeta.size /= 2;
+        return mkfile(name, newMeta);
+    });
+    const newMeta = _.cloneDeep(getMeta(node));
+    return mkdir(getName(node), newChildren, newMeta);
+};
+// END
