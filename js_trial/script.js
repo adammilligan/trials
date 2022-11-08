a68e87ff414d111598160a280e72c94168b5f463
Вадим Валыгин <vadim12388@mail.ru>
2022-11-08 16:08:54 +0300

    acc tree

--- js_trial/script.js
+++ js_trial/script.js
@@ -2323,3 +2323,78 @@ export const compressImages = (node) => {
     return mkdir(getName(node), newChildren, newMeta);
 };
 // END
+
+//
+// findFilesByName.js
+// Реализуйте и экспортируйте по умолчанию функцию, которая принимает на вход файловое дерево и подстроку, а возвращает список файлов, имена которых содержат эту подстроку. Функция должна вернуть полные пути до файлов.
+//
+//     Примеры
+// import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
+// import findFilesByName from '../findFilesByName.js';
+//
+// const tree = mkdir('/', [
+//     mkdir('etc', [
+//         mkdir('apache'),
+//         mkdir('nginx', [
+//             mkfile('nginx.conf', { size: 800 }),
+//         ]),
+//         mkdir('consul', [
+//             mkfile('config.json', { size: 1200 }),
+//             mkfile('data', { size: 8200 }),
+//             mkfile('raft', { size: 80 }),
+//         ]),
+//     ]),
+//     mkfile('hosts', { size: 3500 }),
+//     mkfile('resolve', { size: 1000 }),
+// ]);
+//
+// findFilesByName(tree, 'co');
+// // ['/etc/nginx/nginx.conf', '/etc/consul/config.json']
+// Подсказки
+// Для реализации этой логики вам понадобится аккумулятор, в котором будет храниться путь от корня до текущего узла. При проваливании внутрь директорий к нему добавляется имя текущей директории. В остальном логика работы идентична примеру из теории.
+//     Переменную, содержащую внутри себя путь от корня до текущего узла, можно назвать ancestry.
+//     Для построения путей используйте функцию path.join().
+//     Проверку вхождения строк можно делать с помощью функции str.includes().
+
+// мое
+// @ts-check
+
+// import path from 'path';
+import { isFile, getName, getChildren } from '@hexlet/immutable-fs-trees';
+
+// BEGIN (write your solution here)
+const findFilesByName = (tree, search, path = tree.name) =>
+    (getChildren(tree) || []).reduce((acc, n) => {
+        const p = `${path}${n.name}${isFile(n) ? '' : '/'}`;
+        acc.push(...findFilesByName(n, search, p));
+        return acc;
+    }, tree.type === 'file' && getName(tree).includes(search) ? [path] : []);
+
+export default findFilesByName;
+// END
+
+
+
+// учителя
+
+// @ts-check
+
+import path from 'path';
+import { isFile, getName, getChildren } from '@hexlet/immutable-fs-trees';
+
+// BEGIN (write your solution here)
+const findFilesByName = (tree, string) => {
+    const iter = (node, ancestry) => {
+        const name = getName(node);
+        const newAncestry = path.join(ancestry, name);
+        if (isFile(node)) {
+            return name.includes(string) ? newAncestry : [];
+        }
+        const children = getChildren(node);
+        return children.flatMap((child) => iter(child, newAncestry));
+    };
+    return iter(tree, '');
+};
+
+export default findFilesByName;
+// END
