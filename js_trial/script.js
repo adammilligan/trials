8e76e0717b9db1e9cb8f31030a8f45a9c9ab6cf9
Вадим Валыгин <vadim12388@mail.ru>
2022-11-10 16:09:21 +0300

    massiv as object

--- js_trial/script.js
+++ js_trial/script.js
@@ -2398,3 +2398,51 @@ const findFilesByName = (tree, string) => {
 
 export default findFilesByName;
 // END
+
+//
+// convert.js
+// Реализуйте и экспортируйте по умолчанию функцию, которая принимает на вход массив определённой структуры и возвращает объект, полученный из этого массива.
+//
+//     Массив устроен таким образом, что с помощью него можно представлять ассоциативные массивы. Каждое значение внутри него — это массив из двух элементов, где первый элемент — ключ, а второй — значение. В свою очередь, если значение тоже является массивом, то считается, что это вложенное представление ассоциативного массива. Другими словами, любой массив внутри исходного массива всегда рассматривается как данные, которые нужно конвертировать в объект.
+//
+// convert([]); // {}
+// convert([['key', 'value']]); // { key: 'value' }
+// convert([['key', 'value'], ['key2', 'value2']]); // { key: 'value', key2: 'value2' }
+//
+// convert([
+//     ['key', [['key2', 'anotherValue']]],
+//     ['key2', 'value2']
+// ]);
+// // { key: { key2: 'anotherValue' }, key2: 'value2' }
+
+
+// мое
+// @ts-check
+
+// BEGIN (write your solution here)
+const convert = (arr) => arr.reduce((acc, element) => {
+    if (Array.isArray(element) && element.length) {
+        if (typeof element[0] === 'string') {
+            return { ...acc, [element[0]]: Array.isArray(element[1]) ? convert(element) : element[1] };
+        }
+        return { ...acc, ...convert(element) };
+    }
+    return { ...acc };
+}, {});
+
+export default convert;
+// END
+
+
+// учителя
+// @ts-check
+
+// BEGIN
+const convert = (tree) => tree.reduce((acc, node) => {
+    const [key, value] = node;
+    const newValue = Array.isArray(value) ? convert(value) : value;
+    return { ...acc, [key]: newValue };
+}, {});
+
+export default convert;
+// END
