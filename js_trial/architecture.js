717eef7c011b4527990843107c1b0334c721eab5
Вадим Валыгин <vadim12388@mail.ru>
2023-02-15 17:17:28 +0300

    text interface

--- js_trial/architecture.js
+++ js_trial/architecture.js
@@ -1494,3 +1494,163 @@ export default () => {
   });
 };
 // END
+
+
+// Организация текстов интерфейса
+
+// В этом упражнении вам предстоит запрограммировать мультиязычный счётчик нажатий, состоящий из переключателя языка, кнопки с числом кликов и кнопки сброса счётчика. Начальное состояние:
+//
+//   <div class="btn-group" role="group">
+//     <button type="button" class="btn mb-3 btn-primary">English</button>
+//     <button type="button" class="btn mb-3 btn-outline-primary">Русский</button>
+//   </div>
+// <button type="button" class="btn btn-info mb-3 align-self-center">0 clicks</button>
+// <button type="button" class="btn btn-warning">Reset</button>
+// После двух нажатий по кнопке с классом btn-info и нажатия на кнопку переключения на русский язык:
+//
+//   <div class="btn-group" role="group">
+//     <button type="button" class="btn mb-3 btn-outline-primary">English</button>
+//     <button type="button" class="btn mb-3 btn-primary">Русский</button>
+//   </div>
+// <button type="button" class="btn btn-info mb-3 align-self-center">2 клика</button>
+// <button type="button" class="btn btn-warning">Сбросить</button>
+// src/application.js
+// Экспортируйте функцию по умолчанию, которая реализует всю необходимую логику: работу кнопок счётчика, переключение языка. Тексты должны подставляться через библиотеку i18next.
+//
+//   src/locales/en.js
+// Реализуйте тексты для англоязычной версии приложения.
+//
+//   src/locales/ru.js
+// Реализуйте тексты для русскоязычной версии приложения.
+//
+//   Подсказки
+// Для реализации переводов множественной формы вы можете написать свою функцию для получения нужного ключа по числу или воспользоваться возможностями библиотеки i18next
+// Каждый запуск приложения должен создавать свой собственный инстанс i18next.
+//   Для изменения языка используется функция changeLanguage
+
+
+export default {
+  translation: {
+    // BEGIN (write your solution here)
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    buttons: {
+      counter: {
+        count_one: '{{count}} клик',
+        count_few: '{{count}} клика',
+        count_many: '{{count}} кликов',
+      },
+      reset: 'Сбросить',
+    },
+    // END
+  },
+
+  // @ts-check
+
+  export default {
+    translation: {
+      // BEGIN (write your solution here)
+      languages: {
+        en: 'English',
+        ru: 'Русский',
+      },
+      buttons: {
+        counter: {
+          count_one: '{{count}} click',
+          count_other: '{{count}} clicks',
+        },
+        reset: 'Reset',
+      },
+      // END
+    },
+  };
+
+  // @ts-check
+  /* eslint no-param-reassign: ["error", { "props": false }] */
+
+  import i18n from 'i18next';
+  import onChange from 'on-change';
+  import resources from './locales/index.js';
+
+// BEGIN
+  const languages = ['en', 'ru'];
+
+  const handleSwitchLanguage = (state) => (evt) => {
+    const { lng } = evt.target.dataset;
+
+    state.lng = lng;
+  };
+
+  const render = (container, watchedState, i18nInstance) => {
+    const lngToggler = document.createElement('div');
+    lngToggler.classList.add('btn-group');
+    lngToggler.setAttribute('role', 'group');
+
+    languages.forEach((lng) => {
+      const btn = document.createElement('button');
+      btn.setAttribute('type', 'button');
+      const className = watchedState.lng === lng ? 'btn-primary' : 'btn-outline-primary';
+      btn.classList.add('btn', 'mb-3', className);
+      btn.setAttribute('data-lng', lng);
+      btn.textContent = i18nInstance.t(`languages.${lng}`);
+      btn.addEventListener('click', handleSwitchLanguage(watchedState));
+      lngToggler.appendChild(btn);
+    });
+
+    const counter = document.createElement('button');
+    counter.setAttribute('type', 'button');
+    counter.classList.add('btn', 'btn-info', 'btn-lg', 'mb-3', 'align-self-center');
+    counter.textContent = i18nInstance.t('buttons.counter.count', { count: watchedState.clicksCount });
+    counter.addEventListener('click', () => {
+      watchedState.clicksCount += 1;
+    });
+
+    const reset = document.createElement('button');
+    reset.setAttribute('type', 'button');
+    reset.classList.add('btn', 'btn-warning');
+    reset.textContent = i18nInstance.t('buttons.reset');
+    reset.addEventListener('click', () => {
+      watchedState.clicksCount = 0;
+    });
+
+    container.innerHTML = '';
+    container.append(lngToggler, counter, reset);
+  };
+
+  export default async () => {
+    const defaultLanguage = 'en';
+    // каждый запуск приложения создаёт свой собственный объект i18n и работает с ним,
+    // не меняя глобальный объект.
+    const i18nInstance = i18n.createInstance();
+    await i18nInstance.init({
+      lng: defaultLanguage,
+      debug: false,
+      resources,
+    });
+
+    const state = {
+      lng: defaultLanguage,
+      clicksCount: 0,
+    };
+
+    const container = document.querySelector('.container');
+
+    const watchedState = onChange(state, (path, value) => {
+      switch (path) {
+        // инициализированный объект i18n прокидывается параметром в рендер, чтобы использовать t.
+        case 'lng': i18nInstance.changeLanguage(value).then(() => render(container, watchedState, i18nInstance));
+          break;
+
+        case 'clicksCount': render(container, watchedState, i18nInstance);
+          break;
+
+        default:
+          break;
+      }
+    });
+
+    render(container, watchedState, i18nInstance);
+  };
+// END
